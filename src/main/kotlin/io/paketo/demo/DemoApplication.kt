5e2e0d148acc68e474b025740511fbc2e391dd21
Dmitry Kaznacheev <dmitry.kaznacheev@jetbrains.com>
2025-01-27 00:44:17 +0200

    Db example

--- src/main/kotlin/io/paketo/demo/DemoApplication.kt
+++ src/main/kotlin/io/paketo/demo/DemoApplication.kt
@@ -1,21 +1,57 @@
 package io.paketo.demo
 
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.data.annotation.Id
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import org.springframework.stereotype.Repository
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import java.time.LocalDateTime
 
 @SpringBootApplication
 class DemoApplication
 
+@Repository
+interface AccessLogRepository : JpaRepository<AccessLog, Long> {
+	@Query("SELECT COUNT(a) FROM AccessLog a")
+	fun getTotalRowCount(): Long
+
+	@Query("SELECT MAX(a.access_time) FROM AccessLog a")
+	fun getLatestAccessTime(): LocalDateTime?
+}
+
+@Entity
+data class AccessLog(
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	val id: Long = 0,
+	@Column(name = "access_time")
+	val accessTime: LocalDateTime,
+)
+
 @RestController
 @RequestMapping("/")
-class IndexController {
+class IndexController(private val accessLogRepository: AccessLogRepository) {
 
 	@GetMapping
 	fun sayHello(): String {
-		return "hello!"
+		val accessLog = AccessLog(
+			accessTime = LocalDateTime.now() // Set the current time as access time
+		)
+
+		val totalCount = accessLogRepository.getTotalRowCount()
+		val latestAccessTime = accessLogRepository.getLatestAccessTime()
+
+		accessLogRepository.save(accessLog)
+
+		return "Hi! You are the visitor number $totalCount. Last access was at $latestAccessTime."
 	}
 }
 
